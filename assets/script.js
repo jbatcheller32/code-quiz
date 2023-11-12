cb1668e40e38feba6e491886f9d442e485cb742c
J Mac <jbatcheller123@gmail.com>
2023-11-11 21:41:27 -0700

    The quiz logic has been added, so you can start and answer all the questions.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,23 @@
 var headerEl = document.querySelector('#header');
 var startButtonEl = document.querySelector('#start-btn');
-var firstQuestionEl = document.querySelector('#question-1').style.display = "none";
-var secondQuestionEl = document.querySelector('#question-2').style.display = "none";
-var thirdQuestionEl = document.querySelector('#question-3').style.display = "none";
-var fourthQuestionEl = document.querySelector('#question-4').style.display = "none";
-var fifthQuestionEl = document.querySelector('#question-5').style.display = "none";
+var firstQuestionEl = document.querySelector('#question-container-1').style.display = "none";
+var secondQuestionEl = document.querySelector('#question-container-2').style.display = "none";
+var thirdQuestionEl = document.querySelector('#question-container-3').style.display = "none";
+var fourthQuestionEl = document.querySelector('#question-container-4').style.display = "none";
+var fifthQuestionEl = document.querySelector('#question-container-5').style.display = "none";
+var answerResultEL = document.querySelector('#result1');
+var answerResultELTwo = document.querySelector('#result2');
+var answerResultELThree = document.querySelector('#result3');
+var answerResultELFour = document.querySelector('#result4');
+var answerResultELFive = document.querySelector('#result5');
+var nameSubmit = document.querySelector('#form').style.display = "none";
+var optionEl = document.querySelectorAll('.option');
 var timerEl = document.querySelector('#timer');
 var count = 60;
 
+
 // Main function to get the quiz and timer started
 function startQuiz() {
-
-
 
     var setTimer = setInterval(function () {
 
@@ -20,32 +26,191 @@ function startQuiz() {
 
         if (count === 0) {
             clearInterval(setTimer);
+            return;
 
         }
 
-
     }, 1000);
 
-    // First question
+    // display the next question and hide the previous
 
-    function questionOne() {
+    var displayQuestion = document.querySelector('#question-container-1');
+    displayQuestion.setAttribute('style', "display: block;");
 
-        var choiceArray = ['a reusable block of code designed to perform a particular task', 'a block of code that cannot be used more than once',
-        'functions are not real', 'functions are used to store variables'];
-        
+    var hidePrevious = document.querySelector('header');
+    hidePrevious.setAttribute('style', 'display: none;');
 
-        var displayQuestion = document.querySelector('#question-1');
+
+}
+
+startButtonEl.addEventListener('click', startQuiz);
+
+
+// Question 1
+
+function checkAnswer1(choice) {
+    var selectedAnswer = choice.textContent;
+    var correctAnswer = "a reusable block of code designed to perform a particular task";
+
+
+    if (selectedAnswer === correctAnswer) {
+        answerResultEL.textContent = "Correct!";
+
+        // if you get answer wrong it will subtract 5 seconds off the timer
+
+    } else {
+
+        answerResultEL.textContent = "Incorrect!";
+        count -= 5;
+    }
+
+    //display the next question and hide the previous after 1 second
+
+    setTimeout(function () {
+
+        var displayQuestion = document.querySelector('#question-container-2');
         displayQuestion.setAttribute('style', "display: block;");
 
-        var hidePrevious = document.querySelector('header');
-        hidePrevious.setAttribute('style', 'display: none;');
+
+        var hidePrevious = document.querySelector('#question-container-1');
+        hidePrevious.setAttribute('style', "display: none;");
+
+        if (selectedAnswer === correctAnswer || selectedAnswer !== correctAnswer) {
+            hidePrevious;
+
+        }
+    }, 1000);
 
 
+}
+
+
+// Question 2
+
+function checkAnswer2(choice) {
+    var selectedAnswer = choice.textContent;
+    var correctAnswer = "global scope variables are accessible from any part of your code, and local scope variables are not";
+
+
+    if (selectedAnswer === correctAnswer) {
+        answerResultELTwo.textContent = "Correct!";
+    } else {
+        answerResultELTwo.textContent = "Incorrect!";
+        count -= 5;
     }
-    questionOne();
+
+    setTimeout(function () {
+
+        var displayQuestion = document.querySelector('#question-container-3');
+        displayQuestion.setAttribute('style', "display: block;");
 
 
-    
+        var hidePrevious = document.querySelector('#question-container-2');
+        hidePrevious.setAttribute('style', "display: none;");
+
+        if (selectedAnswer === correctAnswer || selectedAnswer !== correctAnswer) {
+            hidePrevious;
+
+        }
+    }, 1000);
+
+
+}
+
+// Question 3
+
+function checkAnswer3(choice) {
+    var selectedAnswer = choice.textContent;
+    var correctAnswer = "false";
+
+
+    if (selectedAnswer === correctAnswer) {
+        answerResultELThree.textContent = "Correct!";
+    } else {
+        answerResultELThree.textContent = "Incorrect!";
+        count -= 5;
+    }
+
+    setTimeout(function () {
+
+        var displayQuestion = document.querySelector('#question-container-4');
+        displayQuestion.setAttribute('style', "display: block;");
+
+
+        var hidePrevious = document.querySelector('#question-container-3');
+        hidePrevious.setAttribute('style', "display: none;");
+
+        if (selectedAnswer === correctAnswer || selectedAnswer !== correctAnswer) {
+            hidePrevious;
+
+        }
+    }, 1000);
+
+
+}
+
+
+// Question 4 
+
+function checkAnswer4(choice) {
+    var selectedAnswer = choice.textContent;
+    var correctAnswer = "responsive design";
+
+
+    if (selectedAnswer === correctAnswer) {
+        answerResultELFour.textContent = "Correct!";
+    } else {
+        answerResultELFour.textContent = "Incorrect!";
+        count -= 5;
+    }
+
+    setTimeout(function () {
+
+        var displayQuestion = document.querySelector('#question-container-5');
+        displayQuestion.setAttribute('style', "display: block;");
+
+
+        var hidePrevious = document.querySelector('#question-container-4');
+        hidePrevious.setAttribute('style', "display: none;");
+
+        if (selectedAnswer === correctAnswer || selectedAnswer !== correctAnswer) {
+            hidePrevious;
+
+        }
+    }, 1000);
+
+
+}
+
+// Question 5
+
+function checkAnswer5(choice) {
+    var selectedAnswer = choice.textContent;
+    var correctAnswer = "event handlers";
+
+
+    if (selectedAnswer === correctAnswer) {
+        answerResultELFive.textContent = "Correct!";
+    } else {
+        answerResultELFive.textContent = "Incorrect!";
+        count -= 5;
+    }
+
+    setTimeout(function () {
+
+        var displayQuestion = document.querySelector('#form');
+        displayQuestion.setAttribute('style', "display: block;");
+
+
+        var hidePrevious = document.querySelector('#question-container-5');
+        hidePrevious.setAttribute('style', "display: none;");
+
+        if (selectedAnswer === correctAnswer || selectedAnswer !== correctAnswer) {
+            hidePrevious;
+
+        }
+    }, 1000);
+
 
 }
 
@@ -55,7 +220,21 @@ function startQuiz() {
 
 
 
-startButtonEl.addEventListener('click', startQuiz);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
